2a4481e751c739a518055beb2172a376187caa22
leonardolopardo <34845307+leonardolopardo@users.noreply.github.com>
2021-06-14 18:13:16 -0300

    Add files via upload
    Timetracker

---
+++ Mindata/src/main/java/com/mindata/annotation/ExecutionTimeTrackerAdvice.java
@@ -0,0 +1,23 @@
+package com.mindata.annotation;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeTrackerAdvice {
+
+	Logger logger = Logger.getLogger(ExecutionTimeTrackerAdvice.class);
+
+	@Around("@annotation(com.mindata.annotation.TrackExecutionTime)")
+	public Object trackTime(ProceedingJoinPoint pjp) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		Object obj = pjp.proceed();
+		long endTime = System.currentTimeMillis();
+		logger.info("Method name: " + pjp.getSignature() + " time taken to execute : " + (endTime - startTime));
+		return obj;
+	}
+}
